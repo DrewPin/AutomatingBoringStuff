2b0c974f29b168ff54a8a7b602e1aa41fa1e26ca
Drew.Pineider <drew.pineider@gmail.com>
2020-05-25 21:36:22 -0500

    First Commit

---
+++ heythey.py
@@ -0,0 +1,13 @@
+Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> git config --global user.name "Drew.Pineider"
+git config --global user.email "drew.pineider@gmail.com"
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> hey = 2
+>>> they = 3
+>>> hey + they
+5
+>>> 
